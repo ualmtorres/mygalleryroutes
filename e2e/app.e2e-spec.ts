ad7f1c4a02b791c0eb072a79f4a8f8a0f29f91fc
Manolo Torres <=>
2018-03-12 14:41:08 +0100

    Galería de imágenes

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('mygiphy App', () => {
+describe('mygallery App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
